f2814f2f9f42b2861aa8af24833a3c715bb06f29
DESKTOP-KN2GHNS\lukas <lukas.stoecklein@gmail.com>
2018-03-27 23:40:18 +1300

    initial

---
+++ ToggleMaximizeAndResume.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ToggleMaximizeAndResume : MonoBehaviour {
+	EditorWindow gameWindow;
+
+	void Start () {
+		System.Type T = System.Type.GetType ("UnityEditor.GameView,UnityEditor");
+		System.Reflection.MethodInfo GetMainGameView = T.GetMethod ("GetMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+		System.Object Res = GetMainGameView.Invoke (null, null);
+		gameWindow = (EditorWindow) Res;
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.O) && Input.GetKeyDown (KeyCode.P)) {
+			Pause ();
+			ToggleMaximize ();
+			Resume ();
+		}
+	}
+
+	void ToggleMaximize () {
+		gameWindow.maximized = !gameWindow.maximized;
+	}
+
+	void Pause () {
+		EditorApplication.isPaused = true;
+	}
+
+	void Resume () {
+		EditorApplication.isPaused = false;
+	}
+
+}
